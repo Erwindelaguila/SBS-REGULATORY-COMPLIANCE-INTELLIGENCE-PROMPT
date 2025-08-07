c44595ace2b07e11c57bf03002a78f6a9f053f93
Jose Huaman <josehuamanaparcana@bcp.com.pe>
2025-08-07 02:02:23 -0500

    feat: lambda response stream

--- functions/regulatory-compliance-prompt/app.ts
+++ functions/regulatory-compliance-prompt/app.ts
@@ -1,16 +1,14 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { S3Client } from "@aws-sdk/client-s3";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
-import middy from "@middy/core";
-import httpHeaderNormalizer from "@middy/http-header-normalizer";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import pino from "pino";
-import { S3FileStorageClient } from "./source/adapters/s3-file-storage.client";
-import { DynSystemPromptsRepositoryImpl } from "./source/adapters/dyn-system-prompts.repository-impl";
+
 import { BedrockRuntimeClient } from "@aws-sdk/client-bedrock-runtime";
-import { PromptRegulatoryComplianceCommandHandler } from "./source/domain/command-handlers/prompt-regulatory-compliance.command-handler";
+import { Context, LambdaFunctionURLEvent } from "aws-lambda";
+import pino from "pino";
 import { BedrockAIChatClient } from "./source/adapters/bedrock-ai-chat.client";
+import { DynSystemPromptsRepositoryImpl } from "./source/adapters/dyn-system-prompts.repository-impl";
+import { S3FileStorageClient } from "./source/adapters/s3-file-storage.client";
+import { PromptRegulatoryComplianceCommandHandler } from "./source/domain/command-handlers/prompt-regulatory-compliance.command-handler";
 import { PromptRegulatoryComplianceEntrypoint } from "./source/entrypoints/prompt-regulatory-compliance.entrypoint";
 
 /**
@@ -27,16 +25,12 @@ const logger = pino({});
 
 const dynamoDBDocumentClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
-const fileStorageClient = new S3FileStorageClient(
-  new S3Client({}), 
-  process.env.BUCKET_NAME!, 
-  logger
-);
+const fileStorageClient = new S3FileStorageClient(new S3Client({}), process.env.BUCKET_NAME!, logger);
 
 const systemPromptsRepository = new DynSystemPromptsRepositoryImpl(
   dynamoDBDocumentClient,
   process.env.SYSTEM_PROMPTS_TABLE_NAME!,
-  logger
+  logger,
 );
 
 const aiChatClient = new BedrockAIChatClient(
@@ -44,61 +38,41 @@ const aiChatClient = new BedrockAIChatClient(
     region: "us-east-1",
   }),
   process.env.BEDROCK_MODEL_ID!,
-  logger
+  logger,
 );
 
 const promptRegulatoryComplianceCommandHandler = new PromptRegulatoryComplianceCommandHandler(
   fileStorageClient,
   systemPromptsRepository,
   aiChatClient,
-  logger
+  logger,
 );
 
 const promptRegulatoryComplianceEntrypoint = new PromptRegulatoryComplianceEntrypoint(
-  promptRegulatoryComplianceCommandHandler
+  promptRegulatoryComplianceCommandHandler,
 );
 
-export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const body = event.body! as any;
-  try {
+export const handler = awslambda.streamifyResponse(
+  async (event: LambdaFunctionURLEvent, responseStream: awslambda.HttpResponseStream, context: Context) => {
+    const body = JSON.stringify(event.body!) as any;
+    try {
+      const promptRegComplOutPut = await promptRegulatoryComplianceEntrypoint.handleRequest({
+        question: body.question as string,
+        recordKeys: body.recordKeys as string[],
+      });
 
-    const result = await promptRegulatoryComplianceEntrypoint.handleRequest({
-      question: body.question as string,
-      recordKeys: body.recordKeys as string[]
-    });
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(result),
-      headers: {
-        "Access-Control-Allow-Headers": "*",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "*",
+      for await (const chunk of promptRegComplOutPut.result) {
+        logger.debug({ chunk }, "Chunk");
+        responseStream.write(chunk);
       }
-    };
-  } catch (error) {
-    // TODO: handle send error responses
-    if (error instanceof Error) {
-      logger.error({ err: error }, "Error in lambda handler");
+
+      logger.info("Finish writing response");
+      responseStream.end();
+    } catch (error) {
+      // TODO: handle send error responses
+      if (error instanceof Error) {
+        logger.error({ err: error }, "Error in lambda handler");
+      }
     }
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: "Internal Server Error",
-      }),
-      headers: {
-        "Access-Control-Allow-Headers": "*",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "*",
-      }
-    };
-  }
-};
-
-export const handler = middy<
-  APIGatewayProxyEvent, 
-  APIGatewayProxyResult
->({ streamifyResponse: true })
-  .use(httpHeaderNormalizer())
-  .use(httpJsonBodyParser())
-  .handler(lambdaHandler);
+  },
+);
