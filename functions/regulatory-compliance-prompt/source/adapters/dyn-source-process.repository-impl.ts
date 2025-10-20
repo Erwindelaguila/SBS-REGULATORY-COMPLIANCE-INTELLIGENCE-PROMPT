9db379bc7551d9dc928f297ac1f4036d55146066
Henry <elpadredelcordero@gmail.com>
2025-10-20 08:08:51 -0500

    finalizado chatbot garantias

--- functions/regulatory-compliance-prompt/source/adapters/dyn-source-process.repository-impl.ts
+++ functions/regulatory-compliance-prompt/source/adapters/dyn-source-process.repository-impl.ts
@@ -11,48 +11,57 @@ export class DynSourceProcessRepositoryImpl implements SourceProcessRepository {
     }
 
 
-    async getSources(sources: string[]): Promise<string[]> {
-        const finallyResponses=[]
-        for (const source of sources) {
-            let ExclusiveStartKey: Record<string, any> | undefined;
+  async getSources(sources: string[], flow:string): Promise<string[]> {
+    const finallyResponses=[]
+    for (const source of sources) {
+      let ExclusiveStartKey: Record<string, any> | undefined;
 
-            do {
-                const input: QueryCommandInput = {
-                    TableName: this.tableName,
-                    KeyConditionExpression: "#source = :src",
-                    ExpressionAttributeNames: {
-                        "#source": "source", // alias por ser reservada
-                    },
-                    ExpressionAttributeValues: {
-                        ":src": source,
-                    },
-                    ExclusiveStartKey,
-                };
+      do {
+        const input: QueryCommandInput = {
+          TableName: this.tableName,
+          KeyConditionExpression: "#source = :src",
+          ExpressionAttributeNames: {
+            "#source": "source", // alias por ser reservada
+          },
+          ExpressionAttributeValues: {
+            ":src": source,
+          },
+          ExclusiveStartKey,
+        };
 
-                const resp = await this.dynamoDBDocumentClient.send(new QueryCommand(input));
-                ExclusiveStartKey = resp.LastEvaluatedKey;
+        const resp = await this.dynamoDBDocumentClient.send(new QueryCommand(input));
+        ExclusiveStartKey = resp.LastEvaluatedKey;
 
 
-                for (const item of resp.Items ?? []) {
-                    // Verifica si tiene type === 'processed' y existe el campo data
-                    this.logger.debug({ source, item }, "Encontrado item con type=processed y data presente");
-                    finallyResponses.push(this.solveKey(item))
+        for (const item of resp.Items ?? []) {
+          // Verifica si tiene type === 'processed' y existe el campo data
+          this.logger.debug({ source, item }, "Encontrado item con type=processed y data presente");
+          finallyResponses.push(flow==="LETTER"?this.solveKeyWhenIsLetter(item):this.solveKeyWhenIsWarranty(item))
 
-                }
-            } while (ExclusiveStartKey);
-
-            this.logger.debug({ source }, "No se encontró item con type=processed");
         }
-        return finallyResponses.filter(f=>f!=="")
-    }
+      } while (ExclusiveStartKey);
 
-    private solveKey(item:any):string {
-        if (item?.type === "letter-analysis.processed" && item?.data) {
-            console.log("data",item?.data)
-            return item.data?.key ?? "";
-        }
-        return "";
+      this.logger.debug({ source }, "No se encontró item con type=processed");
     }
+    return finallyResponses.filter(f=>f!=="")
+  }
+
+  private solveKeyWhenIsLetter(item:any):string {
+    if (item?.type === "letter-analysis.processed" && item?.data) {
+      console.log("data",item?.data)
+      return item.data?.key ?? "";
+    }
+    return "";
+  }
+
+  private solveKeyWhenIsWarranty(item:any):string {
+    if (item?.type === "warranty-analysis.processed" && item?.data) {
+      console.log("data",item?.data)
+      return item.data?.key ?? "";
+    }
+    return "";
+  }
+
 
 
 }
